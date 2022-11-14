35a15bcedb22ae4c94bcef553924e2065a52825f
dolsen0 <derek.olsen@gmail.com>
2022-11-14 15:59:42 -0500

    created navbar for jewelers of parkland site

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import { Nav } from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <>
+      <Navbar>
+        <Container>
+          <NavbarBrand>Jewelers of Parkland</NavbarBrand>
+          <Nav.Link>Home</Nav.Link>
+          <Nav.Link>About</Nav.Link>
+          <Nav.Link>Contact</Nav.Link>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
