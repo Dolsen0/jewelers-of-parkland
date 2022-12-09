c681c0b41d20ee70de5fbf2045c48253b594ae56
dolsen0 <derek.olsen@gmail.com>
2022-12-09 11:36:21 -0500

    added pictures

--- src/App.js
+++ src/App.js
@@ -9,20 +9,41 @@ function App() {
       <NavBar />
 
       <h2>Jewelers of Parkland</h2>
-      <div className="mainView">
+      <div>
 
-
-          <div>upcoming photo </div>
-          <div> upcoming photo</div>
+      <img
+        src="https://s3-media0.fl.yelpcdn.com/bphoto/DFm7cV00AQbwlUrXctnN4Q/o.jpg"
+        alt=""
+        />
         </div>
-        <h2>About</h2>
-        <p className="about">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste
-          qui, provident explicabo ullam asperiores. Modi, consectetur. Odio
-          sunt quo ducimus.
-        </p>
 
+      <h2 className="topic">About</h2>
+      <p className="about">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste qui,
+        provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
+        ducimus.
+      </p>
+
+      <div>
+        <img
+          src="https://img1.wsimg.com/isteam/ip/0f1a5fc8-9d15-4863-bdb5-2e854ed4eb46/P1040251-0001.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1300,h:800"
+          alt=""
+        />
+      </div>
+      <h2 className="topic">Contact Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste qui,
+        provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
+        ducimus.
+      </p>
+      <div>
+        <img
+          src="https://img1.wsimg.com/isteam/ip/0f1a5fc8-9d15-4863-bdb5-2e854ed4eb46/P1040273.jpg/:/rs=w:1300,h:800"
+          alt=""
+        />
+      </div>
       <Footer />
     </div>
   );
