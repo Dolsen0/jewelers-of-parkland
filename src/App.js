1fd0f7b8cc8f7f5a1738d859fb4004ab1bb65543
dolsen0 <derek.olsen@gmail.com>
2022-12-21 14:13:57 -0500

    update to styling

--- src/App.js
+++ src/App.js
@@ -10,12 +10,11 @@ function App() {
 
       <h2>Jewelers of Parkland</h2>
       <div>
-
-      <img
-        src="https://s3-media0.fl.yelpcdn.com/bphoto/DFm7cV00AQbwlUrXctnN4Q/o.jpg"
-        alt=""
-        />
-        </div>
+        {/* <img
+          src="https://s3-media0.fl.yelpcdn.com/bphoto/DFm7cV00AQbwlUrXctnN4Q/o.jpg"
+          alt=""
+        /> */}
+      </div>
 
       <h2 className="topic">About</h2>
       <p className="about">
@@ -23,14 +22,21 @@ function App() {
         voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste qui,
         provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
         ducimus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste qui,
+        provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
+        ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        voluptates facere dicta, ipsa itaque ipsam nulla. Nostrum non iste qui,
+        provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
+        ducimus.
       </p>
 
-      <div>
+      {/* <div className="parallax">
         <img
           src="https://img1.wsimg.com/isteam/ip/0f1a5fc8-9d15-4863-bdb5-2e854ed4eb46/P1040251-0001.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1300,h:800"
           alt=""
         />
-      </div>
+      </div> */}
       <h2 className="topic">Contact Us</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
@@ -38,12 +44,12 @@ function App() {
         provident explicabo ullam asperiores. Modi, consectetur. Odio sunt quo
         ducimus.
       </p>
-      <div>
+      {/* <div className="parallax">
         <img
           src="https://img1.wsimg.com/isteam/ip/0f1a5fc8-9d15-4863-bdb5-2e854ed4eb46/P1040273.jpg/:/rs=w:1300,h:800"
           alt=""
         />
-      </div>
+      </div> */}
       <Footer />
     </div>
   );
